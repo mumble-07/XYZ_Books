bec0e7445c87c58c88b316b6e57d320467bf0dc6
mumble <andreapauline.deguzman@gmail.com>
2024-03-29 14:14:50 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'static_pages#index'
-  get '/search' => 'static_page#search'
-  get '/books/:isbn' => 'static_page#book_api'
-  get '/convert/:isbn' => 'static_page#isbn_converter'
+  get '/search' => 'static_pages#search'
+  get '/books/:isbn' => 'static_pages#api_book'
+  get '/convert/:isbn' => 'static_pages#convert_isbn'
   
 end

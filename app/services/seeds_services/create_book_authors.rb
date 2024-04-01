ed47d04ebca68e2555a5af4ab70bde9230c0cfda
mumble <andreapauline.deguzman@gmail.com>
2024-04-01 12:02:37 +0800

    SEED SERVICE CODE UPDATE updated seeds for creating book authors to allow multiple authors mapping per book

--- app/services/seeds_services/create_book_authors.rb
+++ app/services/seeds_services/create_book_authors.rb
@@ -6,9 +6,19 @@ module SeedsServices
       @logger.info 'Deleted all data from BookAuthor model'
       
       # Example code to create book authors
-      books.each_with_index do |book, index|
-        author = authors[index % authors.size]
-        BookAuthor.create!(book: book, author: author)
+      book_authors_data = [
+        { book: books[0], authors: [authors[0], authors[1], authors[2]] },
+        { book: books[1], authors: [authors[3]] },
+        { book: books[2], authors: [authors[3]] },
+        { book: books[3], authors: [authors[1], authors[4], authors[5]] },
+        { book: books[4], authors: [authors[6]] }
+        # Add more book-author assignments as needed
+      ]
+      
+      book_authors_data.each do |data|
+        data[:authors].each do |author|
+          BookAuthor.create!(book: data[:book], author: author)
+        end
       end
 
       @logger.info 'BookAuthor has been created'

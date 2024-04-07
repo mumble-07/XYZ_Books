08b92a421e2027ef793ae7cadd342657a612336d
mumble <andreapauline.deguzman@gmail.com>
2024-04-08 05:45:01 +0800

    CONTROLLER UPDATE - fixed my logic on api_books, isbn converter, and search to properly render error pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,22 +3,58 @@ class StaticPagesController < ApplicationController
   def index
   end
 
-  def search
-    parameter = params[:search]
-
+  def convert_isbn
+    parameter = params[:isbn].delete('-')
+  
     if ISBN_Tools.is_valid?(parameter)
-      @results = Book.where("books.isbn_10 LIKE ? OR books.isbn_13 LIKE ?", "%#{parameter}%", "%#{parameter}%")
+      result = if parameter.length == 13
+                 ISBN_Tools.isbn13_to_isbn10(parameter)
+               elsif parameter.length == 10
+                 ISBN_Tools.isbn10_to_isbn13(parameter)
+               end
+  
+      hyphenated_result = hyphenate_isbn(result)
+      render plain: hyphenated_result
     else
       render_error_page(400)
     end
   end
 
+  def hyphenate_isbn(isbn)
+    if isbn.length == 13
+      hyphenated_isbn = ISBN_Tools.hyphenate_isbn13(isbn)
+    elsif isbn.length == 10
+      hyphenated_isbn = ISBN_Tools.hyphenate_isbn10(isbn)
+    end
+  
+    hyphenated_isbn || isbn # Return the hyphenated ISBN or original ISBN if no hyphenation was done
+  end
+
+  def search
+    parameter = params[:search]
+  
+    if ISBN_Tools.is_valid?(parameter)
+      @results = Book.where("books.isbn_10 LIKE ? OR books.isbn_13 LIKE ?", "%#{parameter}%", "%#{parameter}%")
+      
+      if @results.empty?
+        render_error_page(404)
+      end
+    else
+      render_error_page(400)
+    end
+  end
+  
+
   def api_book
     parameter = params[:isbn]
     results = Book.where("books.isbn_10 LIKE ? OR books.isbn_13 LIKE ?", "%#{parameter}%", "%#{parameter}%")
+    
+    puts "Results size: #{results.size}" # Debugging statement
   
     if ISBN_Tools.is_valid_isbn13?(parameter)
-      if results.exists?
+      if results.empty?
+        render_error_page(404)
+      else
         mapped_result = results.map do |result|
           {
             id: result.id,
@@ -33,36 +69,19 @@ class StaticPagesController < ApplicationController
         end
   
         render json: mapped_result
-      else
-        render_error_page(404)
       end
     else
       render_error_page(400)
     end
   end
   
+    
   private
   
   def author_full_name(author)
     [author.first_name, author.middle_name, author.last_name].compact.join(" ")
   end
-
-  def convert_isbn
-    parameter = params[:isbn].delete('-')
-
-    if ISBN_Tools.is_valid?(parameter)
-      result = if parameter.length == 13
-                 ISBN_Tools.isbn13_to_isbn10(parameter)
-               elsif parameter.length == 10
-                 ISBN_Tools.isbn10_to_isbn13(parameter)
-               end
-
-      hyphenated_result = hyphenate_isbn(result)
-      render body: hyphenated_result
-    else
-      render_error_page(400)
-    end
-  end
+  
 
   private
 
@@ -85,11 +104,4 @@ class StaticPagesController < ApplicationController
     end
   end
 
-  def hyphenate_isbn(isbn)
-    if isbn.length == 13
-      ISBN_Tools.hyphenate_isbn13(isbn).to_s
-    elsif isbn.length == 10
-      ISBN_Tools.hyphenate_isbn10(isbn).to_s
-    end
-  end
 end

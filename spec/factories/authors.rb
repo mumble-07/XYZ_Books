8305ab9e6bfe18bfee2fbc2627a681dc1ddc24fe
mumble <andreapauline.deguzman@gmail.com>
2024-04-08 10:57:25 +0800

    FACTORIES AUTHOR - added associations for books and authors made sure to do fail test first

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,8 +1,17 @@
 FactoryBot.define do
   factory :author do
-    first_name { "Andi" }
-    middle_name { "D" }
-    last_name { "De Guzman" }
-    book_id { rand(1..100) }       
+    first_name { "Joel" }
+    middle_name { "" }
+    last_name { "Hartse" }
+    
+    # Using a transient attribute to generate associations with books
+    transient do
+      books_count { 1 } # Default to 1 book association
+    end
+
+    # After creating an author, associate it with books
+    after(:create) do |author, evaluator|
+      create_list(:book, evaluator.books_count, authors: [author])
+    end
   end
 end
